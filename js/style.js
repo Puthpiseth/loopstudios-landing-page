17b597b89f1f14542a9921bf2ada1d91a987e513
puthpiseth <puthpiseth_tun@yahoo.com>
2021-03-18 08:13:42 +0100

    Pull

---
+++ js/style.js
@@ -0,0 +1,7 @@
+const main_menu = document.querySelector('.main-menu');
+const close_menu = document.querySelector('.icon-close');
+const open_menu = document.querySelector('.icon-hamburger');
+
+open_menu.addEventListener("click", ()=> {
+    
+})
